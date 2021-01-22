a772c2c15ed2b3001af4bcd1ba09d48d685f2897
gnardinosaur <github email address>
2021-01-21 22:38:01 -0500

    moved SearchBar to Header component

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React, { useState } from 'react';
 import './App.global.scss';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
 
 function App() {
-  const [addSearchBar, setAddSearchBar] = useState(false)
-
-  function handleAddSearch() {
-    setAddSearchBar(!addSearchBar)
-  };
 
   return (
-    <div>
-    <Header handleAddSearch={handleAddSearch} />
-    {addSearchBar ? <SearchBar /> : <div></div>}
-    </div>
+    <Header />
   );
 }
 
